46ab36198b2c1d8119752450c3042302fa2bd1b9
mulong <759401524@qq.com>
2021-07-01 09:00:10 +0800

    add files

---
+++ lib/gcc/aarch64-linux-gnu/10.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../configure --prefix= --target=aarch64-linux-gnu --enable-languages=c,c++,fortran --with-sysroot=/aarch64-linux-gnu/libc --with-build-sysroot=/tmp/cross-pi-gcc-10.2.0-64/aarch64-linux-gnu/libc --with-arch=armv8-a+fp+simd --disable-multilib";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "arch", "armv8-a+fp+simd" } };
